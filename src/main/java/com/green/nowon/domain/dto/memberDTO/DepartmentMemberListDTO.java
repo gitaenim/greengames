51a149507075bb7837b52245a48d8821a76d7fd7
sinjung <sinjung@192.168.219.100>
2023-01-27 00:07:37 +0900

    id->email로 변경

--- src/main/java/com/green/nowon/domain/dto/memberDTO/DepartmentMemberListDTO.java
+++ src/main/java/com/green/nowon/domain/dto/memberDTO/DepartmentMemberListDTO.java
@@ -14,7 +14,7 @@ public class DepartmentMemberListDTO {
 	private String name;
 	private String pass;
 	private String phone;
-	
+	private String email;
 	private String profileUrl;
 	
 	//private String pName;
@@ -26,6 +26,7 @@ public class DepartmentMemberListDTO {
 		this.name=e.getName();
 		this.pass = e.getPass();
 		this.phone=e.getPhone();
+		this.email=e.getEmail();
 		//this.profileUrl=e.getProfile().getUrl()+e.getProfile().getNewName();
 		if(e.getProfile()!=null) {
 			this.profileUrl=e.getProfile().getUrl()+e.getProfile().getNewName();
